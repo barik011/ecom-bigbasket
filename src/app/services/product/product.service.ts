6ace9b375991eff14f9471e6f02ba06265bfa6f5
Github Projects <barik011@gmail.com>
2025-05-16 17:31:08 +0530

    remove proxy

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import constants from 'constants';
 import { Constant } from '../constant/constant';
 
 @Injectable({
